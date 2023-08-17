61b4ec7edd2d9a037adb62d5c0a7b6115882feb4
Ujw@l-007 <138686275+Ujwal-007@users.noreply.github.com>
2023-08-17 10:43:09 +0545

    Add files via upload
    basic

---
+++ operator js/opper.js
@@ -0,0 +1,10 @@
+function operator() {
+    let x = 5;
+    document.getElementById("operator").innerHTML= (x=="5");   //comparator operator ('==')
+}
+function greta (){
+    let x="A";
+    let y="B";
+    let result=x>y;
+    document.getElementById("operator").innerHTML=result;
+}
